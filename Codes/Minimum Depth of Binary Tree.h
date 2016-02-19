7288f6e380f7365875a6499207043db17ef9b7eb
Allen Z Young <allenzyoungazy@gmail.com>
2016-02-19 19:18:12 +0800

    Create Minimum Depth of Binary Tree.h

---
+++ Codes/Minimum Depth of Binary Tree.h
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution { // recursive solution， O(n) time O(logN) space
+public:
+	int minDepth(TreeNode* root) {
+		if (root == NULL) return 0;
+		if (root->left == NULL) return 1 + minDepth(root->right);
+		if (root->right == NULL) return 1 + minDepth(root->left);
+		return 1 + min(minDepth(root->left), minDepth(root->right));     
+  }
+};
